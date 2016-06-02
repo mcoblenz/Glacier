cafa6d8b536c65e017b32cb168e32c3cd21734ec
Michael Coblenz <mcoblenz@gmail.com>
2016-06-02 10:47:02 -0400

    Added readonly test.

---
+++ tests/glacier/ReadOnlyClass.java
@@ -0,0 +1,23 @@
+import edu.cmu.cs.glacier.qual.*;
+import java.lang.String;
+
+// Classes can't be annotated ReadOnly in their declarations; @ReadOnly is only for method parameters.
+//::error: (glacier.readonly.class)
+@ReadOnly public class ReadOnlyClass {
+}
+
+class ReadOnlyMethodClass {
+    @ReadOnly ReadOnlyClass roc;
+    
+    //::error: (type.invalid)
+    void takeReadOnlyString(@ReadOnly String foo) {}
+    void takeReadOnlyArray(String @ReadOnly [] foo) {
+	//::error: (glacier.assignment.array)
+	foo[0] = "Hello, world!";
+    }
+
+    void takeImmutableArray(String @Immutable [] foo) {
+	//::error: (glacier.assignment.array)
+	foo[0] = "Hello, world!";
+    }
+}
