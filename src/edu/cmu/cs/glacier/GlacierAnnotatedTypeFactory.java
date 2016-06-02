b8ef8cf6f93cef805b45c03923d747f576ae67a6
Michael Coblenz <mcoblenz@gmail.com>
2016-06-02 11:19:12 -0400

    Moved annotations on String and Number from the type factory to the stub file.

--- src/edu/cmu/cs/glacier/GlacierAnnotatedTypeFactory.java
+++ src/edu/cmu/cs/glacier/GlacierAnnotatedTypeFactory.java
@@ -77,16 +77,7 @@ public class GlacierAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
         return selfType;
     }
 	
-	
-	private static boolean isWhitelistedImmutableClass(Element element) {
-		if (element.asType().toString().equals("java.lang.String") ||
-			element.asType().toString().equals("java.lang.Number")) {
-			return true;
-		}
-		// TODO: add more classes.
-		return false;
-	}
-	
+		
 	private static boolean isAutoboxedImmutableClass(Types types, AnnotatedTypeMirror type) {
 		// Surely there is a better API for doing this than having to try/catch.
 		try {
@@ -168,8 +159,7 @@ public class GlacierAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
             // are no annotations from that hierarchy already on the type.
 
             if (classElt != null) {
-            	boolean isHardCodedImmutable = isAutoboxedImmutableClass(p.types, type) || 
-            			isWhitelistedImmutableClass(classElt);
+            	boolean isHardCodedImmutable = isAutoboxedImmutableClass(p.types, type);
             	
             	if (isHardCodedImmutable && !type.isAnnotatedInHierarchy(p.READ_ONLY)) {
                 	type.addAnnotation(Immutable.class);
