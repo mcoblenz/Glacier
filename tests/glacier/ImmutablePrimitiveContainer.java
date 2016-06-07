a50334d79708475dd73f58f5dd35956428f8de6c
Michael Coblenz <mcoblenz@gmail.com>
2016-06-07 11:24:14 -0400

    Added new tests. StaticAssignment currently fails because I still need to add a special case in Glacier to disallow assignment to static variables in constructors.

---
+++ tests/glacier/ImmutablePrimitiveContainer.java
@@ -0,0 +1,11 @@
+import edu.cmu.cs.glacier.qual.Immutable;
+
+@Immutable
+public class ImmutablePrimitiveContainer {
+    int x;
+
+    public void setX(int x) {
+	//::error: (glacier.assignment)
+	this.x = x;
+    }
+}
---
+++ tests/glacier/StaticAssignment.java
@@ -0,0 +1,17 @@
+import edu.cmu.cs.glacier.qual.Immutable;
+
+@Immutable
+public class StaticAssignment {
+    public static int GLOBAL = 1; // OK, no error here
+
+    StaticAssignment() {
+	//::error: (glacier.assignment)
+	GLOBAL = 2;
+    }
+    
+    public void setStatics () {
+	//::error: (glacier.assignment)
+	GLOBAL = 42;
+    }
+    
+}
