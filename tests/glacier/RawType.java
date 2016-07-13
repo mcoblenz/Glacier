808de2c4bfc40a16800c612d2c331d994b642313
Michael Coblenz <mcoblenz@gmail.com>
2016-07-13 12:12:13 -0400

    Added test for raw types.

---
+++ tests/glacier/RawType.java
@@ -0,0 +1,10 @@
+import java.util.Collections;
+import java.util.Vector;
+
+@SuppressWarnings("unchecked")
+public class RawType {
+    public void foo() {
+	Vector v2 = new Vector();
+	Collections.sort(v2);
+    }
+}
